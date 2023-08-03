fda3f700396eca59a3be5c2e0d1805c650fdeba8
marcabounader92 <marcabounader92@outlook.com>
2023-08-04 02:21:41 +0300

    added Models

---
+++ BackEnd/app/Models/Student_Project.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student_Project extends Model
+{
+    use HasFactory;
+}
