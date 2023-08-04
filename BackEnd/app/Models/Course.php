dfa43aad79525a8b6c8110b8fa967b22e27f93bc
marcabounader92 <marcabounader92@outlook.com>
2023-08-04 14:39:18 +0300

    added course schedules

--- BackEnd/app/Models/Course.php
+++ BackEnd/app/Models/Course.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     use HasFactory;
+
+    function schedules(): HasMany
+    {
+        return $this->hasMany(Schedule::class);
+
+    }
 }
