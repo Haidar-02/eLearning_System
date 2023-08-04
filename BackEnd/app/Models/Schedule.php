cc9ecaf325e3404978ae6caca97cf03c9542e0e6
marcabounader92 <marcabounader92@outlook.com>
2023-08-04 14:39:30 +0300

    get schedule tasks,materials,sessions

--- BackEnd/app/Models/Schedule.php
+++ BackEnd/app/Models/Schedule.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Schedule extends Model
 {
     use HasFactory;
+
+    function materials(): HasMany
+    {
+        return $this->hasMany(Course_Material::class);
+    }
+
+    function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+
+    }
+
+    function sessions(): HasMany
+    {
+        return $this->hasMany(Session::class);
+
+    }
 }
