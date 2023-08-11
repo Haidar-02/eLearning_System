1adc944fd07e45f415ee80fbf49867098695eab7
marcabounader92 <marcabounader92@outlook.com>
2023-08-11 16:42:15 +0300

    Server: modified common controller imports and functions

--- BackEnd/app/Http/Controllers/CommonController.php
+++ BackEnd/app/Http/Controllers/CommonController.php
@@ -2,13 +2,115 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\GroupProject;
 use App\Models\Message;
+use App\Models\Schedule;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CommonController extends Controller
 {
+    
+    public function getCourseSchedules($course_id){
+        try{
+            $course=Course::find($course_id);
+            $schedules=$course->schedules;
+            return response()->json([
+                'status' => 'success',
+                'schedules'=>$schedules
+            ]);
+        } catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }    
+    }
+    public function getScheduleMaterials($schedule_id){
+        try{
+
+            $schedule=Schedule::where([['id','=',$schedule_id]])->first();
+            $materials=$schedule->materials;
+
+            return response()->json([
+                'status' => 'success',
+                'materials'=>$materials
+            ]);
+        } catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }    
+    }
+    public function getScheduleTasks($schedule_id){
+    try{
+        $schedule=Schedule::where([['id','=',$schedule_id]])->first();
+        $tasks=$schedule->tasks;
+
+        return response()->json([
+            'status' => 'success',
+            'tasks'=>$tasks
+        ]);
+    } catch(Exception $e){
+        return response()->json([
+            'status' => 'error',
+            'message' => $e->getMessage()
+        ]);
+    } 
+    }
+
+    public function getScheduleSessions($schedule_id){
+        try{
+            $schedule=Schedule::where([['id','=',$schedule_id]])->first();
+            $sessions=$schedule->sessions;
+    
+            return response()->json([
+                'status' => 'success',
+                'sessions'=>$sessions
+            ]);
+        } catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        } 
+    }
+
+    public function getScheduleProjects($schedule_id){
+        try{
+            $schedule=Schedule::where([['id','=',$schedule_id]])->first();
+            $projects=$schedule->projects;
+    
+            return response()->json([
+                'status' => 'success',
+                'projects'=>$projects
+            ]);
+        } catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        } 
+    }
+
+    public function getProjectMembers($project_id){
+        try{
+            $project=GroupProject::where([['id','=',$project_id]])->first();
+            $members=$project->members()->with('info')->get();
+            return response()->json([
+                'status' => 'success',
+                'projects'=>$members
+            ]);
+        } catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        } 
+    }
     public function sendMessage(Request $request){
         try{
         $message=new Message;
@@ -40,6 +142,9 @@ class CommonController extends Controller
                 'message' => $e->getMessage()
             ]);
         } 
+
+    }
+    public function getCourseDiscussion($course_id){
 
     }
 }
