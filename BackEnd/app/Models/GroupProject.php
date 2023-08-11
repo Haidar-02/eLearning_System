77711fdb4f6083609b10c01426892f305087e1bf
marcabounader92 <marcabounader92@outlook.com>
2023-08-11 13:03:43 +0300

    Server: added group project get members

--- BackEnd/app/Models/GroupProject.php
+++ BackEnd/app/Models/GroupProject.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class GroupProject extends Model
 {
@@ -11,4 +12,10 @@ class GroupProject extends Model
 
     public $timestamps = false;
 
+    public function members(): HasMany
+    {
+        return $this->hasMany(StudentProject::class);
+
+    }
 }
+
