832aa07f786dc9662bb645a529cbd9abe5f50cc4
marcabounader92 <marcabounader92@outlook.com>
2023-08-11 13:04:13 +0300

    Server: added project members get info

--- BackEnd/app/Models/StudentProject.php
+++ BackEnd/app/Models/StudentProject.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class StudentProject extends Model
 {
@@ -11,4 +12,9 @@ class StudentProject extends Model
 
     public $timestamps = false;
 
+    public function info(): HasOne
+    {
+        return $this->hasOne(User::class,"id","student_id");
+
+    }
 }
