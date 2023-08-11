f577cb616f825a5983adfb086f6ff6d367113a7d
marcabounader92 <marcabounader92@outlook.com>
2023-08-11 16:42:53 +0300

    Server: added session-attendance relation

--- BackEnd/app/Models/Session.php
+++ BackEnd/app/Models/Session.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Session extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
-
+    public function attendances(): BelongsToMany
+    {
+        return $this->belongsToMany(Attendance::class);
+    }
 }
