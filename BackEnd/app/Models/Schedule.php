cdb2679f83a01a6853420f05563a6b022736c354
marcabounader92 <marcabounader92@outlook.com>
2023-08-12 16:18:55 +0300

    Server: removed schedule timestamp

--- BackEnd/app/Models/Schedule.php
+++ BackEnd/app/Models/Schedule.php
@@ -10,6 +10,8 @@ class Schedule extends Model
 {
     use HasFactory;
 
+    public $timestamps = false;
+
     public function materials(): HasMany
     {
         return $this->hasMany(CourseMaterial::class);
