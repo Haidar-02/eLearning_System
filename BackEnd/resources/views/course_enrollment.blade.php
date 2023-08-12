cd2ed8571a76c4d86a0875dea7ce1b73695eb7d0
HilalBodon <hilal_bodon@hotmail.com>
2023-08-12 18:29:53 +0300

    rebasing

--- BackEnd/resources/views/course_enrollment.blade.php
+++ BackEnd/resources/views/course_enrollment.blade.php
@@ -7,12 +7,12 @@
     <div>
         <p>Hello,</p>
     <p>You have successfully enrolled in the following course:</p>
-    <!-- <p>Course Name: {{ $course->name }}</p>
+    <p>Course Name: {{ $course->name }}</p>
     <p>Course Code: {{ $course->code }}</p>
     <p>Course Description: {{ $course->description }}</p>
     <p>Course Start Date: {{ $course->start_date }}</p>
-    <p>Course End Date: {{ $course->end_date }}</p> -->
+    <p>Course End Date: {{ $course->end_date }}</p>
     <p>Thank you for choosing our platform for your learning needs!</p>
-    </div>
+</div>
 </body>
 </html>
