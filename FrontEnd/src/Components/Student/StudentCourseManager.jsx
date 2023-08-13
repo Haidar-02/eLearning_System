2ceae3e0ca7f6c4a8cfc3faed608e9ddf9babf25
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-13 16:08:59 +0300

    FrontEnd <> modified StudentCourseManager

--- FrontEnd/src/Components/Student/StudentCourseManager.jsx
+++ FrontEnd/src/Components/Student/StudentCourseManager.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
-import CourseCard from '../../Components/Courses/CourseCard';
 import Button from '../../Components/Common/Button';
 import { getAllCourses } from '../../helpers/common.helpers';
 import CourseAdd from '../../Components/Courses/CourseAdd';
+import StudentCourseCard from './StudentCourseCard';
 
 const StudentCourseManager = () => {
   const [courses, setCourses] = useState();
@@ -27,7 +27,11 @@ const StudentCourseManager = () => {
 
       {courses &&
         courses.map((course, index) => (
-          <CourseCard key={index} course={course} setCourses={setCourses} />
+          <StudentCourseCard
+            key={index}
+            course={course}
+            setCourses={setCourses}
+          />
         ))}
     </div>
   );
