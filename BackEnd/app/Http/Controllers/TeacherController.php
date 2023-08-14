33fe91ee3ad401e5070089a0972b38c88132e8dd
marcabounader92 <marcabounader92@outlook.com>
2023-08-14 15:37:25 +0300

    Server: echo member

--- BackEnd/app/Http/Controllers/TeacherController.php
+++ BackEnd/app/Http/Controllers/TeacherController.php
@@ -331,6 +331,7 @@ class TeacherController extends Controller
                 $member->student_id = $student["id"];
                 $member->project_id = $group->id;
                 $member->save();
+                echo $member;
             }
             $group=$group->with('members')->first();
 
