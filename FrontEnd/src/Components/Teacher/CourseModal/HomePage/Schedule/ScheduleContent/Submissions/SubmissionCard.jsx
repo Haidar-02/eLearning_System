1a8db1079ac34846b3290aace6ac02376e755184
marcabounader92 <marcabounader92@outlook.com>
2023-08-14 20:06:15 +0300

    Client: added submission card

--- FrontEnd/src/Components/Teacher/CourseModal/HomePage/Schedule/ScheduleContent/Submissions/SubmissionCard.jsx
+++ FrontEnd/src/Components/Teacher/CourseModal/HomePage/Schedule/ScheduleContent/Submissions/SubmissionCard.jsx
@@ -1,86 +1,52 @@
-import React, { useState } from 'react';
-import CustomInput from '../../../../../Inputs/CustomInput';
-import Modal from '../../../../../Common/Modal';
-import { addScheduleTask } from '../../../../../../helpers/Teacher.helpers';
-import Button from '../../../../../Common/Button';
-import TextArea from '../../../../../Inputs/TextArea';
-const initialState = {
-  title: '',
-  description: '',
-  due_date: '',
-  task_type: '1'
-};
-const TaskAdd = ({ setTaskAdd, setTasks,schedule_id,course_id}) => {
-  const [state, setState] = useState(initialState);
-  const [error, setError] = useState();
-  function inputHandler(e) {
-    const { name, value } = e.target;
-    setState((prev) => ({ ...prev, [name]: value }));
-  } 
-  async function handleSave() {
-    const { data, errorMessages, message } = await addScheduleTask({...state,course_id,schedule_id});
-    if (errorMessages) {
-      setError(errorMessages[0]);
-      return;
-    } else if (message) {
-      setError(message);
-      return;
-    }
-    if (data) {
-      console.log(data);
-      setTasks((prev) => {
-        return [data.task, ...prev];
-      });
+import React, { useState } from "react";
+import Button from "../../../../../Common/Button";
+import { modify } from "../../../../../../helpers/Teacher.helpers";
+import Submissions from "./Submissions/Submissions";
 
-      setTaskAdd(false);
-    }
-  }
+const TaskCard = ({ task,setTasks,setSubmissions}) => {
+  const {
+    id,
+    schedule_id,
+    course_id,
+    title,
+    max_score,
+    teacher_id,
+    description,
+    due_date,
+    task_type
+  } = task;
 
-  const { title, description, due_date,task_type } = state;
-  return (
-    <Modal
-    setShow={setTaskAdd}
-    className=" flex flex-col p-5 justify-center rounded-2xl gap-5 min-w-[400px]"
-    >
-    <CustomInput
-      label="Title"
-      name="title"
-      value={title}
-      onChange={inputHandler}
-    />
-    <TextArea
-      label="Description"
-      name="description"
-      value={description}
-      onChange={inputHandler}
-    />
-    <input type='datetime-local' name='due_date' value={due_date} onChange={inputHandler}/>
-    <label htmlFor="task_type">Task type</label>
-    <select name="task_type" onChange={inputHandler}>
-      <option value="1">Assignment</option>
-      <option value="2">Quiz</option>
-    </select>
-    <div className="error text-sm text-red-500 ">{error}</div>
-
-    <div className="button-container flex gap-5 justify-end">
+//   const updateSubmission = async () => {
+//     const res = await removeScheduleTask(id);
+//     setTasks((prev)=>prev.filter(i => i.id !== id));
+// };
+  return ( 
+      <div className="flex flex-col p-3 border gap-3 rounded-md transition-colors hover:bg-slate-200">
+      <div
+        className="course-title cursor-pointer" onClick={()=>setSubmissions({show:true,task_id:id})}
+      >
+        <span className="text-md font-bold uppercase hover:underline">
+          {title}
+        </span>
+      </div>
+      <div className="content monster text-xs flex flex-col gap-5 cursor-default">
+        <div className="description">
+          <span className="font-semibold">Description: </span>
+          {description}
+        </div>
+        <div className="enrollment-limit">
+          <span className="font-semibold">Due date: </span>
+          {due_date}
+        </div>
+      </div>
       <Button
-        text="Create"
-        className=" text-[16px] bg-green text-white p-3 self-end"
-        onClick={() => {
-          handleSave();
-        }}
-      />
-      <Button
-        text="cancel"
-        onClick={() => {
-          setTaskAdd(false);
-        }}
-        className="text-[16px] bg-transparent text-cyan-600  p-3 self-end "
-      />
+                onClick={() => deleteTask()}
+                text="Delete"
+                className="p-0 bg-transparent text-xl text-black"
+        />
     </div>
-    </Modal>
+ 
   );
 };
 
-export default TaskAdd;
-
+export default TaskCard;
