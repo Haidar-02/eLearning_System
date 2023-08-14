2867a5276c7ec7caf4dc90f6168dc9cf266faf19
HaidarMestrah <10120319@mu.edu.lb>
2023-08-14 07:33:33 +0300

    client:message screen

--- FrontEnd/src/Components/Teacher/MessagesList.jsx
+++ FrontEnd/src/Components/Teacher/MessagesList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import MessagePreview from "./MessagePreview";
+import mailIcon from "../../assets/icons/mail-icon.svg";
 
 const MessagesList = () => {
   return (
     <div className="flex-col align-middle">
       <h1 className="border-b border-gray-300 w-100% font-semibold mb-8">
-        Messages From
+        <img src={mailIcon} alt="" className="float-left w-5 mr-5" />
+        My Messages
       </h1>
+
+      {/* Map messages here */}
       <MessagePreview />
     </div>
   );
