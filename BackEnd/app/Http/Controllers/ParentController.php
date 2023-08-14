1a64f1a7431645a423e0eec7a9521c0f7d3c2d35
HaidarMestrah <10120319@mu.edu.lb>
2023-08-14 23:56:23 +0300

    Server: Created Functions

--- BackEnd/app/Http/Controllers/ParentController.php
+++ BackEnd/app/Http/Controllers/ParentController.php
@@ -8,27 +8,21 @@ class ParentController extends Controller
 {
     public function getChildCourses()
     {
-        // Logic to fetch the list of courses enrolled by the child
     }
     public function getChildTeachers()
     {
-        // Logic to fetch a list of teachers for the child's courses
     }
     public function getChildAttendance()
     {
-        // Logic to fetch attendance records for the child
     }
     public function getChildCourseSchedules()
     {
-        // Logic to fetch course schedules for the child
     }
     public function getTeacherConferenceSlots($teacherId)
     {
-        // Logic to fetch available time slots for parent-teacher conferences
     }
-    
+
     public function scheduleConferenceWithTeacher($teacherId, Request $request)
     {
-        // Logic to schedule a parent-teacher conference
     }
 }
