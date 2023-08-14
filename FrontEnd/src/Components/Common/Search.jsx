364418be6e000648f7a8a8be0e931f24c7b3d42d
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-14 15:00:45 +0300

    FrontEnd <> added code and  Fixed bugs in search component

--- FrontEnd/src/Components/Common/Search.jsx
+++ FrontEnd/src/Components/Common/Search.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { search } from '../helpers/user.helpers';
 import CustomInput from '../Inputs/CustomInput';
+import {
+  getMessages,
+  search,
+  getMessagesById,
+} from '../../helpers/common.helpers';
 
-const Search = () => {
+const Search = ({ setSelectedMessages, teacher }) => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState();
-
   const timeout = useRef();
 
   const changeHandler = (e) => {
@@ -15,35 +18,45 @@ const Search = () => {
 
   async function handleDebounceSearch(e) {
     clearTimeout(timeout.current);
-    if (!search) return;
+    if (!query.trim()) {
+      setResults('');
+      return;
+    }
 
     timeout.current = setTimeout(async () => {
-      const users = await search(2, query.trim());
+      const users = await search({ userType: 2, search: query.trim() });
       setResults(users);
     }, 600);
   }
 
   return (
-    <div className="w-[400px] p-5 absolute h-full top-0 left-[60px] bg-white  border-x-[1px] ">
-      <div className="searchbar">
-        <div className="input-container flex flex-col  border-b-[1px] py-5 px-2 gap-5 ">
-          <CustomInput
-            label="search"
-            type="search"
-            className="bg-slate-300 rounded w-[200px] shadow appearance-none border  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={query}
-            onChange={(e) => {
-              changeHandler(e);
-              handleDebounceSearch(search);
-            }}
-          />
-        </div>
+    <div className="w-[250px] h-fit bg-cyan-light rounded-2xl p-5 flex flex-col gap-5  ">
+      <div className="input-container flex flex-col  justify-center gap-5 ">
+        <CustomInput
+          label="search"
+          type="search"
+          className=" bg-white rounded w-[200px] shadow appearance-none border  py-2 px-3  text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={query}
+          onChange={(e) => {
+            changeHandler(e);
+            handleDebounceSearch(search);
+          }}
+        />
       </div>
-      <div className="results border-red-500 flex flex-col gap-5">
+
+      <div className="results flex flex-col gap-5 max-h-[200px] overflow-auto">
         {query &&
           results &&
           results.data.users.map((user, index) => (
-            <span key={index} className="">
+            <span
+              key={index}
+              onClick={async () => {
+                const messages = await getMessagesById(user.id);
+                setSelectedMessages(messages);
+                teacher(user.id);
+              }}
+              className=" border-b-[1px] py-2 "
+            >
               {user.name}
             </span>
           ))}
