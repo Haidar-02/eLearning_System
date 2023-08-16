f6ab1bd7dd33bdfd88544323e095da67751162ea
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-16 03:35:32 +0300

    Frontend <> added helper functions

--- FrontEnd/src/helpers/helpers.js
+++ FrontEnd/src/helpers/helpers.js
@@ -15,4 +15,20 @@ const getUser = () => {
   const user = JSON.parse(localStorage.getItem('user'));
   return user;
 };
-export { toBase64, replaceObjectById, getUser };
+
+function getUnique(submissions) {
+  const uniqueSubmissionsMap = new Map();
+  submissions.forEach((submission) => {
+    uniqueSubmissionsMap.set(submission.task_id, submission);
+  });
+  const uniqueSubmissions = Array.from(uniqueSubmissionsMap.values());
+  return uniqueSubmissions;
+}
+
+function compareDates(taskA, taskB) {
+  const dateA = new Date(taskA.submission_date);
+  const dateB = new Date(taskB.submission_date);
+  return dateA - dateB;
+}
+
+export { toBase64, replaceObjectById, getUser, getUnique, compareDates };
