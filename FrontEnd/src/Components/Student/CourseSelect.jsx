901af331e1b470d966aafebcf32f7a619159c306
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-16 07:20:04 +0300

    Frontend <> small bug fix

--- FrontEnd/src/Components/Student/CourseSelect.jsx
+++ FrontEnd/src/Components/Student/CourseSelect.jsx
@@ -4,11 +4,7 @@ import {
   getStudentProgressDetails,
 } from '../../helpers/student.helpers';
 import { getUnique, compareDates } from '../../helpers/helpers';
-// ['date', 'grades'],
-// ['2014', 1000],
-// ['2015', 1170],
-// ['2016', 660],
-// ['2017', 1030],
+
 const CourseSelect = ({ setTasks, setTotal, setGraphData }) => {
   const [courses, setCourses] = useState();
 
@@ -34,32 +30,30 @@ const CourseSelect = ({ setTasks, setTotal, setGraphData }) => {
       <span className="title  text-base font-bold ">Courses</span>
       {courses &&
         courses.map((course, index) => (
-          <>
-            <div className="flex flex-col  text-xs ">
-              <span
-                className="border-t py-2 cursor-pointer hover:bg-gray-100"
-                key={index}
-                onClick={async () => {
-                  const res = await getStudentProgressDetails(course.id);
-                  const {
-                    submitted_tasks,
-                    graded_tasks,
-                    ungraded_tasks,
-                    total_tasks,
-                  } = res.data;
-                  console.log(res.data);
-                  if (submitted_tasks) {
-                    const submitted = getUnique(submitted_tasks);
-                    setTasks(submitted);
-                    setTotal(total_tasks);
-                    populateData(graded_tasks);
-                  }
-                }}
-              >
-                {course.title}
-              </span>
-            </div>
-          </>
+          <div key={index} className="flex flex-col  text-xs ">
+            <span
+              className="border-t py-2 cursor-pointer hover:bg-gray-100"
+              key={index}
+              onClick={async () => {
+                const res = await getStudentProgressDetails(course.id);
+                const {
+                  submitted_tasks,
+                  graded_tasks,
+                  ungraded_tasks,
+                  total_tasks,
+                } = res.data;
+                console.log(res.data);
+                if (submitted_tasks) {
+                  const submitted = getUnique(submitted_tasks);
+                  setTasks(submitted);
+                  setTotal(total_tasks);
+                  populateData(graded_tasks);
+                }
+              }}
+            >
+              {course.title}
+            </span>
+          </div>
         ))}
     </div>
   );
