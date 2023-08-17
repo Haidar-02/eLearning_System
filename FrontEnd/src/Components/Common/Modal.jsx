a9fdee1fff60c37a1282f2745b9423d703bac4a5
marcabounader92 <marcabounader92@outlook.com>
2023-08-17 09:56:05 +0300

    Client: fixed modal style

--- FrontEnd/src/Components/Common/Modal.jsx
+++ FrontEnd/src/Components/Common/Modal.jsx
@@ -12,7 +12,7 @@ const Modal = ({ children, className, setShow, backDropColor }) => {
         className={`bg fixed top-0 z-10 left-0 w-[100vw] h-full bg-opacity-20 backdrop-blur-sm drop-shadow-lg ${backDropColor} `}
       ></div>
       <div
-        className={`z-10  transition-opacity absolute top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 ${className} `}
+        className={`z-30  transition-opacity fixed top-1/2 left-1/2 transform  -translate-x-1/2 -translate-y-1/2 ${className} `}
         
       >
         {children}
